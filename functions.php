a2135473382f16e0df860db067d1e32cffa7fae7
vahabm74 <vahabmohammadi74@gmail.com>
2022-08-19 02:39:53 +0430

    Add main functions file to functions.php file

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+/*
+ - Locate main functions : inc/main-functions.php
+ - Please add your custom code to main-functions.php
+ - Don't write code on this file
+ */
+require_once ('inc/main-functions.php');
