0704229ab8aee02e4910835961ab2903e9b97a6e
vahabm74 <vahabmohammadi74@gmail.com>
2023-01-11 18:45:40 +0330

    remove all

--- functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
- - Locate main functions : inc/main-functions.php
- - Please add your custom code to main-functions.php
- - Don't write code on this file
- */
-require_once ('inc/main-functions.php');
